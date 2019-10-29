93842c2a64c6e8fb5bb2bf7f1c9d010252197911
abhijeetpandit <abhijeetpandit2008@gmail.com>
2019-10-29 21:12:31 +0530

    added login and signup page

--- website-trial4/src/app/app-routing.module.ts
+++ website-trial4/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignupComponentComponent } from './signup-component/signup-component.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponentComponent},
+  {path: 'signup', component: SignupComponentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- website-trial4/src/app/app.module.ts
+++ website-trial4/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
+import { SignupComponentComponent } from './signup-component/signup-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponentComponent,
+    SignupComponentComponent
   ],
   imports: [
     BrowserModule,
